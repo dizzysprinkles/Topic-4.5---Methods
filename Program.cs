dac57a1c314b40ea0665bb99631e73c59d6eb73f
Zoey Hamm <339622581@GoTVDSB.ca>
2025-02-14 13:41:58 -0500

    Finished Tutorial

--- Program.cs
+++ Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-           
+            //Console.WriteLine("Press ENTER for a joke: ");
+            //Console.ReadLine();
+            //Joke(50);
+
+            int bugs;
+            Console.WriteLine("How many bugs are in your code?");
+            Int32.TryParse(Console.ReadLine(), out bugs);
+            Console.WriteLine();
+            Joke(bugs);
+            Console.ReadLine();
+        }
+
+        public static void Joke(int numBugs)
+        {
+            Console.WriteLine(numBugs + " little bugs in the code,");
+            Thread.Sleep(500);
+            Console.WriteLine(numBugs + " little bugs.");
+            Thread.Sleep(500);
+            Console.WriteLine("Fix a bug, run it again,");
+            Thread.Sleep(500);
+            Console.WriteLine((++numBugs) + " little bugs in the code.");
         }
 
         public static void Joke()
