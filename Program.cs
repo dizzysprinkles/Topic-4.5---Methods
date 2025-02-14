ef5bf320423474247bc3c60898e131467b7988a0
Zoey Hamm <339622581@GoTVDSB.ca>
2025-02-14 13:34:58 -0500

    Started assignment

--- Program.cs
+++ Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           
+        }
+
+        public static void Joke()
+        {
+            Console.WriteLine("99 little bugs in the code,");
+            Thread.Sleep(500);
+            Console.WriteLine("99 little bugs.");
+            Thread.Sleep(500);
+            Console.WriteLine("Fix a bug, run it again,");
+            Thread.Sleep(500);
+            Console.WriteLine("100 little bugs in the code.");
         }
     }
 }
